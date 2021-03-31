ed083fea3ba22cf390ee547ddcfa2a0a750bc1a8
cnorm88 <cnorm88@gmail.com>
2021-03-31 18:20:52 -0400

    commit tree b61152664dc8f0d3b817f4798bcd8a86027aa2fa

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Mr. Chocolate', 8, 'M', 'Light Brown', 'sassy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "Brown", "Excitable", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabita", 9, "F", "Dark Brown", "Gentle", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 12, "M", "Blue-Black", "Mad", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "Black", "Playful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "M", "Blonde", "Cranky", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 2, "F", "Cinnamon", "Kind", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 15, "M", "White", "Unfriendly", 0);
